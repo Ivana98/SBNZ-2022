b1632e96f140c18aaf386bed212f3319955fbf58
Ivana Marosevic <i.marosevic@hotmail.com>
2022-05-24 11:03:37 +0200

    domaci 2 - ispravke

--- drools-integration/workout-recommender/src/main/java/sbnz/integracija/workoutrecommender/model/InputData.java
+++ drools-integration/workout-recommender/src/main/java/sbnz/integracija/workoutrecommender/model/InputData.java
@@ -1,9 +1,6 @@
 package sbnz.integracija.workoutrecommender.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import sbnz.integracija.workoutrecommender.model.enums.ActivityLevel;
 import sbnz.integracija.workoutrecommender.model.enums.BodyType;
 import sbnz.integracija.workoutrecommender.model.enums.Equipment;
--- drools-integration/workout-recommender/src/main/java/sbnz/integracija/workoutrecommender/model/dto/OutputDataDTO.java
+++ drools-integration/workout-recommender/src/main/java/sbnz/integracija/workoutrecommender/model/dto/OutputDataDTO.java
@@ -1,10 +1,12 @@
 package sbnz.integracija.workoutrecommender.model.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class OutputDataDTO {
     private List<WorkoutDTO> recommendedWorkouts;
     private String advice;
